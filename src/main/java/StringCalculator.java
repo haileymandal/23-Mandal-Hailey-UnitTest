c52b9e8256c498c9993415ca9e6d2e03de1d99cc
Hailey Mandal <mandalhailey@gmail.com>
2023-06-20 15:17:50 -0700

    changes to StringCalculator, added Sara's starting code

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,13 +1,19 @@
 
 public class StringCalculator {
-    public static int add(final String numbers){
-        int returnValue = 0;
+    public static int add(String numbers) {
+        int returnValue=0;
         String[] numbersArray = numbers.split(",|\n");
-        for(String number : numbersArray){
-            if(!number.trim().isEmpty()){
-                returnValue += Integer.parseInt(number.trim());
+        if (numbersArray.length > 2) {
+            throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+        } else {
+            for (String number : numbersArray) {
+                if (!number.trim().isEmpty()) {
+                    returnValue+=Integer.parseInt(number.trim()); // If it is not a number, parseInt will throw an exception
+                }
             }
         }
         return returnValue;
+
     }
 }
+
