08facf0eefb0fc4940bdae9348b3c4dcdc3fb762
Hailey Mandal <mandalhailey@gmail.com>
2023-06-20 15:29:58 -0700

    committed out BeforeEach/AfterEach

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -6,19 +6,19 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StringCalculatorTest {
     String sc;
 
-    @BeforeEach
-    public void setUp() {
-        String sc = "";
-    }
-
-    @AfterEach
-    public void tearDown() {
-        String sc = "";
-    }
+//    @BeforeEach
+//    public void setUp() {
+//        String sc = "";
+//    }
+//
+//    @AfterEach
+//    public void tearDown() {
+//        String sc = "";
+//    }
 
     @Test
     public void stringCalculator_addingWithNegativeNumber_throwException(){
-        sc = "3,-2,1";
+        sc = "-5,1";
 
         assertThrows(IllegalArgumentException.class, ()->
                 StringCalculator.add(sc));
