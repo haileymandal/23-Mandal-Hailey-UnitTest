54c7ecfb5f345629326cf6ac29120c1d64c702c7
Hailey Mandal <mandalhailey@gmail.com>
2023-06-20 15:22:53 -0700

    added BeforeEach and AfterEach method

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,8 +1,20 @@
 import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringCalculatorTest {
-    String sc = "";
+    String sc;
+
+    @BeforeEach
+    public void setUp() {
+        String sc = "";
+    }
+
+    @AfterEach
+    public void tearDown() {
+        String sc = "";
+    }
 
     @Test
     public void stringCalculator_addingWithNegativeNumber_throwException(){
